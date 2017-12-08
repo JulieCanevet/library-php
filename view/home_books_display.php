4426e5b9d7771fc9adf578941251b3c4593e2c60
Julie.Canevet <julie.canevet@hotmail.fr>
2017-12-08 14:15:48 +0100

    add link

--- view/home_books_display.php
+++ view/home_books_display.php
@@ -1,18 +1,18 @@
 <section id="home">       
   <?php
   if(isset($books)){
-      foreach ($books as $key => $value) {
+      foreach ($books as $book) {
   ?>
       <div class="books">
-          <h3><?php echo $value['title'] ?></h3>
+          <h3><?php echo $book['title'] ?></h3>
 
-          <h6>Auteur : <?php echo $value['author'] ?></h6>
-          <h6>Résumé : <?php echo $value['summary'] ?></h6>
-          <h6>Catégories : <?php echo $value['category'] ?></h6>
-          <?php if($value['availability'] == 1){ 
+          <h6>Auteur : <?php echo $book['author'] ?></h6>
+          <h6>Résumé : <?php echo $book['summary'] ?></h6>
+          <h6>Catégories : <?php echo $book['category'] ?></h6>
+          <?php if($book['availability'] == 1){ 
             ?> <h6 class="disponible">Disponible</h6> 
               <form class="card-button" id="edit" method="POST" action="index.php">     
-                <input type="hidden" name="id_book" value="<?php echo $value['id_book']?>">
+                <input type="hidden" name="id_book" value="<?php echo $book['id_book']?>">
                  <div class="form-group">
                    <label>Livre emprunté par</label>
                    <select name="edition">
@@ -29,13 +29,13 @@
             else{
               ?> <h6 class="indisponible">Indisponible</h6>
                 <form class="card-button" id="edit" method="POST" action="index.php">
-                  <input type="hidden" name="id_book" value="<?php echo $value['id_book']?>">
+                  <input type="hidden" name="id_book" value="<?php echo $book['id_book']?>">
                   <button type="submit" name="returned">Rendu</button>
                </form>
 
               <?php } ?>
               <div class="voir">
-                <a href="detail.php?id_book=<?php echo $value['id_book']?>&amp;available=<?php echo $value['availability']?>" class="card-link">Voir</a>
+                <a href="detail.php?id_book=<?php echo $book['id_book']?>&amp;available=<?php echo $book['availability']?>" class="card-link">Voir</a>
               </div>
      </div>
 <?php }} ?>
